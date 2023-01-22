c2a7c77ec48d3cbdea5f060049b43e801b609697
Surav Shrestha <surav_02@live.com>
2023-01-22 11:35:30 +0545

    File upload frontend design layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,17 +6,34 @@ import "@fontsource/roboto/700.css";
 import Navbar from "./components/NavBar";
 import SignupForm from "./components/SignupForm";
 import LoginForm from "./components/LoginForm";
+import Home from "./components/Home";
+
+import { setUser } from "./reducers/loggedUserReducer";
+
+import userService from "./services/user";
 
 import { Routes, Route } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+
+import React, { useEffect } from "react";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const userFromStorage = userService.getUser();
+    dispatch(setUser(userFromStorage));
+  }, [dispatch]);
+
+  const loggedUser = useSelector((state) => state.loggedUser);
+
   return (
     <div>
       <Navbar />
 
       <section className="section">
         <Routes>
-          <Route path="/" element={<LoginForm />} />
+          <Route path="/" element={loggedUser ? <Home /> : <LoginForm />} />
           <Route path="/signup" element={<SignupForm />} />
         </Routes>
       </section>
--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,28 +1,83 @@
-import * as React from "react";
+import { setNotification } from "../reducers/notificationReducer";
+import { setUser } from "../reducers/loggedUserReducer";
+import userService from "../services/user";
+
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
 
-export default function ButtonAppBar() {
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+export default function NavBar() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const loggedUser = useSelector((state) => state.loggedUser);
+
+  const handleLogout = () => {
+    userService.clearUser();
+
+    dispatch(
+      setNotification({
+        msg: `User ${loggedUser.username} logged out.`,
+        error: false,
+      })
+    );
+
+    dispatch(setUser(null));
+
+    navigate("/");
+  };
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
-        <Toolbar>
-          <Typography
-            variant="h4"
-            component="a"
-            href="/"
-            color="inherit"
-            sx={{
-              flexGrow: 1,
-              fontFamily: "Helvetica",
-              textAlign: "center",
-            }}
-          >
-            DERMA.AI
-          </Typography>
-        </Toolbar>
+        <Container>
+          <Toolbar>
+            <Typography
+              variant="h4"
+              component="a"
+              href="/"
+              color="inherit"
+              sx={{
+                flexGrow: 1,
+                fontFamily: "Helvetica",
+                ":hover": {
+                  color: "#fff",
+                },
+              }}
+            >
+              DERMA.AI
+            </Typography>
+
+            <Typography
+              color="inherit"
+              sx={{
+                fontFamily: "Helvetica",
+                textAlign: "right",
+                fontSize: "1.3rem",
+              }}
+            >
+              {loggedUser && loggedUser.username + " | "}
+            </Typography>
+
+            {loggedUser && (
+              <Box>
+                <Button sx={{ color: "#fff", fontSize: 16 }}>History</Button>
+                <Button
+                  sx={{ color: "#fff", fontSize: 16 }}
+                  onClick={handleLogout}
+                >
+                  Log out
+                </Button>
+              </Box>
+            )}
+          </Toolbar>
+        </Container>
       </AppBar>
     </Box>
   );
