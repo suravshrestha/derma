aadf3150a5bad083ded917839d7a0eac3e491bd9
Surav Shrestha <surav_02@live.com>
2023-01-23 15:09:45 +0545

    Add result display frontend design layout

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,3 +1,5 @@
+import Result from "./Result";
+
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import CircularProgress from "@mui/material/CircularProgress";
@@ -8,49 +10,53 @@ const Home = () => {
   const [loading, setLoading] = useState(false);
 
   return (
-    <Grid
-      container
-      spacing={2}
-      sx={{ textAlign: "center" }}
-      direction="column"
-      alignItems="center"
-      justifyContent="center"
-    >
-      <Grid item xs={12}>
-        <Typography variant="h4">Add a photo to scan</Typography>
-        <span class="bulma-loader-mixin"></span>
+    <Grid container spacing={2} alignItems="center" justifyContent="center">
+      <Grid item xs={4} sm={3}>
+        <Grid item xs={12}>
+          <Typography variant="h5" align="center" sx={{ marginBottom: 2 }}>
+            Add a photo to scan
+          </Typography>
+        </Grid>
+
+        <Grid item xs={12}>
+          <div className="file is-centered is-boxed is-medium">
+            <label className="file-label">
+              <input
+                className="file-input"
+                type="file"
+                name="resume"
+                onChange={(e) => {
+                  setLoading(true);
+                  if (e.target.files.length > 0) {
+                    console.log("uploading file: ", e.target.files[0].name);
+                  }
+                }}
+              />
+
+              {loading ? (
+                <div className="file-cta">
+                  <CircularProgress
+                    sx={{ margin: 5 }}
+                    size={50}
+                    thickness={4}
+                  />
+                  <Typography variant="h5">Processsing...</Typography>
+                </div>
+              ) : (
+                <div className="file-cta">
+                  <span className="file-icon icon-text">
+                    <i className="fas fa-upload"></i>
+                  </span>
+                  <span className="file-label">Upload photo</span>
+                </div>
+              )}
+            </label>
+          </div>
+        </Grid>
       </Grid>
 
-      <Grid item xs={12}>
-        <div className="file is-centered is-boxed is-large">
-          <label className="file-label">
-            <input
-              className="file-input"
-              type="file"
-              name="resume"
-              onChange={(e) => {
-                setLoading(true);
-                if (e.target.files.length > 0) {
-                  console.log("uploading file: ", e.target.files[0].name);
-                }
-              }}
-            />
-
-            {loading ? (
-              <div className="file-cta">
-                <CircularProgress sx={{ margin: 5 }} size={50} thickness={4} />
-                <Typography variant="h5">Processsing...</Typography>
-              </div>
-            ) : (
-              <div className="file-cta">
-                <span className="file-icon icon-text">
-                  <i className="fas fa-upload"></i>
-                </span>
-                <span className="file-label">Upload photo</span>
-              </div>
-            )}
-          </label>
-        </div>
+      <Grid item xs={8} sm={8}>
+        <Result />
       </Grid>
     </Grid>
   );
